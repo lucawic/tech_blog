0ebe66f2a04495bc3e03a44b832b2ab49164f309
lucawic <lucaswick@email.arizona.edu>
2021-10-04 07:25:01 -0700

    added user model and created package.json with dependencies

--- controller/api/user-routes.js
+++ controller/api/user-routes.js
@@ -1,3 +1,18 @@
-const router = require('express').Router();
+const router = require ('express').Router();
 
-const userRoutes = require('');
+const {User, Post} = require('../../models');
+
+router.get('/', (req,res)=> {
+    User.findAll({
+        attributes:{
+            exclude:['password']
+        }
+    })
+    .then(dbUserData =>{
+        res.json(dbUserData);
+    })
+    .catch(err=> {
+        console.log(err);
+        res.status(500).json(err);
+    })
+ });
