3ebed7446db1c3553f25911817bc228a09f5d8c2
lucawic <lucaswick@email.arizona.edu>
2021-10-04 06:10:40 -0700

    added user routes and server connect

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL){
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+}else{
+
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_PW, {
+        host:'localhost',
+        dialect: 'mysql',
+        port: 3006
+    })
+}
+
+module.exports = sequelize;
