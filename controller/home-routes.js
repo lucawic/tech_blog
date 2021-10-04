beb859cd65ea899546b50a980ff242f3b6e1de7d
lucawic <lucaswick@email.arizona.edu>
2021-10-04 15:08:25 -0700

    fixing routes

---
+++ controller/home-routes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    res.render('homepage');
+});
+
+router.get('/', (req, res) => {
+    res.render('journal');
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+
+    res.render('login');
+});
+
+router.get('/signup', (req, res)=>{
+    res.render('signup');
+});
+router.get('/posts', (req, res)=>{
+    res.render('posts')
+})
+
+router.get('/posts/id', (req, res) =>{
+    res.render('posts')
+});
+  
+  module.exports = router;
+  
--- controller/index.js
+++ controller/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
+const postPageRoutes = require('./post-page-routes');
 
+router.use('/api', apiRoutes);
+router.use('/posts', postPageRoutes);
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
