0f1721ace9667e4a1b9dffe858fe8999bebb7ff8
lucawic <lucaswick@email.arizona.edu>
2021-10-04 20:17:32 -0700

    adding login page/signup form

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,9 +4,10 @@ router.get('/', (req, res) => {
     res.render('homepage');
 });
 
-router.get('/', (req, res) => {
+/* router.get('/', (req, res) => {
     res.render('journal');
 });
+*/
 
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
---
+++ public/javascript/login.js
@@ -0,0 +1,4 @@
+function signupFormHandler(event) {
+    event.preventDefault();
+}
+document.querySelector('.signup-form'.addEventListener('submit', signupFormHandler));
